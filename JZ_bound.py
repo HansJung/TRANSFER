d9970ecf0af41544489ff1cfac10eadee8b4582e
Hans Jung <yhansjung@gmail.com>
2018-07-11 18:34:08 +0900

    180711 comment added

--- JZ_bound.py
+++ JZ_bound.py
@@ -3,53 +3,50 @@ from DataGen import DataGen
 from StoExp import StoExp
 
 class JZ_bound(object):
+    '''
+    Upper and lower bound (Justin's bound)
+    '''
     def __init__(self):
         return None
 
     def sepOBS(self, OBS, D):
+        '''
+        Separate data frame (X,Y,Z) to X,Y,Z
+        :param OBS: (X,Y,Z)
+        :param D: dimension of Z
+        :return: X,Y,Z
+        '''
         X = OBS['X']
         Y = OBS['Y']
-        Z = OBS[list(range(D))]
+        Z = OBS[list(range(D))] # multi-dimensional z is indexed by 0,1,2,...,d
         return [X, Y, Z]
 
     def JZ_bounds(self, pl, OBS, D, N):
+        '''
+        Computing JZ bound for given policy (pl)
+        :param pl: given policy
+        :param OBS: (X,Y,Z)
+        :param D: dimension of Z
+        :param N: number of samples
+        :return:
+        '''
         [X, Y, Z] = self.sepOBS(OBS, D)
-        pl_proba = pl.predict_proba(Z)
-        pi_val = []
+        pl_proba = pl.predict_proba(Z) # list of [pi(X=0|zi),pi(X=1|zi)] for each i = 1,2,...,N
+        pi_val = [] # list of pi(xi|zi)
 
         for idx in range(N):
-            xi = int(X[idx])
-            proba_elem = pl_proba[idx]
-            pi_val.append(proba_elem[xi])
-        pi_val = np.array(pi_val)
+            xi = int(X[idx]) # each xi
+            proba_elem = pl_proba[idx] # [pi(X=0|zi),pi(X=1|zi)]
+            pi_val.append(proba_elem[xi]) # if xi=0, pi(X=0|zi) // if xi=1, pi(X=1|zi) // i.e., pi(xi|zi)
+        pi_val = np.array(pi_val) # list of pi(xi|zi)
 
-        Li = np.mean(Y * pi_val)
-        Hi = 1 + Li - np.mean(pi_val)
+        Li = np.mean(Y * pi_val) # approximated lower bound Li
+        Hi = 1 + Li - np.mean(pi_val) # approximated upper bound Hi
         return [Li, Hi]
 
 
 if __name__ == "__main__":
-    # Parameter configuration
-    seed_num = np.random.randint(10000000)
-    D = 3
-    N = 3000
-    T = 5000
-
-    # Generating Observation data
-    datagen = DataGen(D,N,seed_num)
-
-    ## Note that Z is binary for simplicity
-    OBS = datagen.obs_data()
-    [X,Y,Z] = sepOBS(OBS, D)
-
-    ## Example of Policy
-    Sto = StoExp(D)
-    poly_logit = Sto.Logit(X,Z)
-    [Li, Hi] = JZ_bounds(poly_logit, OBS, D, N)
-
-    ## Policy intervention answer
-    poly_INTV = datagen.poly_intv_data(poly_logit)
-    Y_pi = np.mean(poly_INTV['Y'])
+    print('Hello World')
 
 
 
--- StoExp.py
+++ StoExp.py
@@ -1,62 +1,82 @@
 import numpy as np
-import pandas as pd
 import random
 
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from xgboost import XGBClassifier
 
 class StoExp(object):
+    '''
+    Set of policies
+    '''
     def __init__(self, D):
         self.D = D
 
-    def Construct_XZ(self, DF):
-        Z = DF[list(range(self.D))]
-        X = DF['X']
+    def sepOBS(self, OBS):
+        '''
+        Separate data frame (X,Y,Z) to X,Y,Z
+        :param OBS: (X,Y,Z)
+        :param D: dimension of Z
+        :return: X,Y,Z
+        '''
+        X = OBS['X']
+        Y = OBS['Y']
+        Z = OBS[list(range(self.D))] # multi-dimensional z is indexed by 0,1,2,...,d
+        return [X, Y, Z]
+
+    def Construct_XZ(self, OBS):
+        '''
+
+        :param DF: Data Frame of (X,Y,Z)
+        :return: (X,Z)
+        '''
+        Z = OBS[list(range(self.D))]
+        X = OBS['X']
         return [Z, X]
 
-    def Trun_by_Y(self, DF, goodprop, criterion):
-        DF_good = list(DF[DF['Y'] > criterion].index)
-        DF_bad = list(DF[DF['Y'] <= criterion].index)
-
-        N = min(len(DF_good), len(DF_bad))
-        N_good = round(N*goodprop)
-        N_bad = N-N_good
-        sampled_good_idx = random.sample(DF_good, N_good)
-        sampled_bad_idx = random.sample(DF_bad, N_bad)
-        sampled_idx = sampled_good_idx + sampled_bad_idx
-
-        return DF.ix[sampled_idx]
-
     def Logit(self, X, Z):
+        '''
+        :return: pi(X|Z), where pi is a logistic regression model
+        '''
         Z_train, Z_test, X_train, X_test = train_test_split(Z, X, test_size=0.1, random_state=0)
         logit_reg = LogisticRegression()
         logit_reg.fit(Z_train, X_train)
         return logit_reg
 
     def SVM_linear(self, X, Z):
+        '''
+        :return: pi(X|Z), where pi is a linear SVM
+        '''
         Z_train, Z_test, X_train, X_test = train_test_split(Z, X, test_size=0.1, random_state=0)
         svmcls = svm.SVC(kernel='linear', probability=True)
         svmcls.fit(Z_train, X_train)
         return svmcls
 
     def SVM_rbf(self, X, Z):
+        '''
+        :return: pi(X|Z), where pi is a RBF SVM
+        '''
         Z_train, Z_test, X_train, X_test = train_test_split(Z, X, test_size=0.1, random_state=0)
         svmcls = svm.SVC(kernel='rbf', probability=True)
         svmcls.fit(Z_train, X_train)
         return svmcls
 
     def MLP(self, X, Z):
+        '''
+        :return: pi(X|Z), where pi is a Multi-layered perceptron (MLP) neural network
+        '''
         Z_train, Z_test, X_train, X_test = train_test_split(Z, X, test_size=0.1, random_state=0)
         mlpcls = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(10, 3), random_state=1)
         mlpcls.fit(Z_train, X_train)
         return mlpcls
 
     def LDA(self, X, Z):
+        '''
+        :return: pi(X|Z), where pi is a Linear discriminant analysis
+        '''
         Z_train, Z_test, X_train, X_test = train_test_split(Z, X, test_size=0.1, random_state=0)
         ldacls = LinearDiscriminantAnalysis()
         ldacls.fit(Z_train, X_train)
@@ -64,17 +84,11 @@ class StoExp(object):
 
 
     def XGB(self, X, Z):
+        '''
+        :return: pi(X|Z), where pi is a XGboost
+        '''
         Z_train, Z_test, X_train, X_test = train_test_split(Z, X, test_size=0.1, random_state=0)
         xgbcls = XGBClassifier(max_depth=10)
         xgbcls.fit(Z_train, X_train)
         return xgbcls
 
-    def Compute_C(self, obspolicy, target_policy, Z):
-        obsprob = obspolicy.predict_proba(Z)
-        targetprob = target_policy.predict_proba(Z)
-        N = len(Z)
-        sum_entropy = 0
-
-        for idx in range(N):
-            sum_entropy += obsprob[idx][0] * np.log(1/(targetprob[idx][0])) + obsprob[idx][1] * np.log(1/(targetprob[idx][1]))
-        return sum_entropy / N
