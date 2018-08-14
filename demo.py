8f431df3febd91a1b904ef1ca6308f971c17de26
Hans Jung <yhansjung@gmail.com>
2018-08-14 16:38:44 -0400

    180814
    Data generation for policy Case 2

--- demo.py
+++ demo.py
@@ -3,6 +3,9 @@ from sklearn import preprocessing
 import numpy as np
 import copy
 from scipy import stats
+from scipy.stats import ttest_ind_from_stats
+import itertools
+
 
 def IST_LabelEncoder(IST, ColName):
     le = preprocessing.LabelEncoder()
@@ -23,6 +26,15 @@ def ThreeCategorize(df):
     df_copy[df_copy <= df_q[0]] = 0
     df_copy[(df_copy> df_q[0]) & (df_copy<= df_q[1])] = 1
     df_copy[(df_copy > df_q[1])] = 2
+
+    return df_copy
+
+def BinaryCategorize(df):
+    df_copy = copy.copy(df)
+    df_q = list(df.quantile([0.5]))
+
+    df_copy[df_copy <= df_q[0]] = 0
+    df_copy[df_copy > df_q[0]] = 1
 
     return df_copy
 
@@ -90,7 +102,7 @@ def IndexifyDisc(IST):
 def ContToDisc(IST):
     # Discretize the continuous variable
     continuous_variable = ['RSBP', 'AGE', 'RDELAY']  # list(set(chosen_variables) - set(discrete_variables))
-    IST['AGE'] = ThreeCategorize(IST['AGE'])
+    IST['AGE'] = BinaryCategorize(IST['AGE'])
     IST['RSBP'] = ThreeCategorize(IST['RSBP'])
     IST['RDELAY'] = ThreeCategorize(IST['RDELAY'])
     return IST
@@ -131,8 +143,8 @@ def GenEXP(IST,sample_N = 10000, remember_seed = 3141693719):
 
 def GenOBS(EXP, seed_obs = 1):
     np.random.seed(seed_obs)
-    weight_sick = 0.05
-    weight_treatment = 0.95
+    weight_sick = 0.02
+    weight_treatment = 0.98
 
     sample_list = []
 
@@ -143,14 +155,14 @@ def GenOBS(EXP, seed_obs = 1):
 
         if elem_treat == 0:
             if elem_EXPD < 0.7:
-                prob = 0.1
+                prob = 0.2
             else:
-                prob = 0.9
+                prob = 0.8
         else:
             if elem_EXPD < 0.7:
-                prob = 0.9
+                prob = 0.8
             else:
-                prob = 0.1
+                prob = 0.2
 
         selection_prob = np.dot([weight_sick, weight_treatment],[prob, 1-elem_treat])
 
@@ -163,7 +175,7 @@ def GenOBS(EXP, seed_obs = 1):
     return OBS
 
 def HideCovarOBS(EXP,OBS):
-    selected_covariates = ['AGE', 'RATRIAL', 'RVISINF', 'RXASP', 'Y']
+    selected_covariates = ['AGE', 'SEX', 'RXASP', 'Y']
 
     ## Resulting dataset
     EXP = EXP[selected_covariates]
@@ -171,13 +183,43 @@ def HideCovarOBS(EXP,OBS):
     return [EXP,OBS]
 
 def ComputeBound(OBS,X):
+    Px0 = len(OBS[OBS[X]==0])/len(OBS)
+    Px1 = len(OBS[OBS[X]==1])/len(OBS)
+
+    # Lx0 = np.mean((OBS[X] == 0) * OBS['Y'])
+    # Lx1 = np.mean((OBS[X] == 1) * OBS['Y'])
+
+    EY_x0 = np.mean(OBS[OBS[X] == 0]['Y'])
+    EY_x1 = np.mean(OBS[OBS[X] == 1]['Y'])
+    Lx0 = EY_x0 * Px0
+    Lx1 = EY_x1 * Px1
+
+    Hx0 = Lx0 + Px1
+    Hx1 = Lx1 + Px0
+
+    return [[Lx0,Lx1],[Hx0,Hx1]]
+
+def EmpiricalComputeBound(OBS,X,delta):
+    N = len(OBS)
+    delta = delta/2
+    fn = np.sqrt(((2 * N) ** (-1)) * (np.log(4) - np.log(delta)))
+
+    Px0 = len(OBS[OBS[X] == 0]) / len(OBS)
+    Px1 = len(OBS[OBS[X] == 1]) / len(OBS)
+
     Lx0 = np.mean((OBS[X] == 0) * OBS['Y'])
     Lx1 = np.mean((OBS[X] == 1) * OBS['Y'])
 
-    Hx0 = Lx0 + np.mean((OBS[X] == 1))
-    Hx1 = Lx1 + np.mean((OBS[X] == 0))
+    Lx0 = max(0,Lx0 - fn)
+    Lx1 = max(0,Lx1 - fn)
 
-    return [[Lx0,Lx1],[Hx0,Hx1]]
+    Hx0 = Lx0 + Px1
+    Hx1 = Lx1 + Px0
+
+    Hx0 = min(Hx0 + fn,1)
+    Hx1 = min(Hx1 + fn,1)
+
+    return [[Lx0, Lx1], [Hx0, Hx1]]
 
 def GroundTruth(EXP,X):
     Ux0 = np.mean(EXP[EXP[X]==0]['Y'])
@@ -189,40 +231,182 @@ def CheckCase2(HB,U):
     Ux0, Ux1 = U
 
     if Ux0 < Ux1:
-        if Ux1 < hx0:
+        if hx0 < Ux1:
             return True
         else:
             return False
     else:
-        if Ux0 < hx1:
+        if hx1 < Ux0:
             return True
         else:
             return False
 
-# Data load
-IST = pd.read_csv('IST.csv')
-X = 'RXASP'
+def ChangeRXASPtoX(df,idx_X=3):
+    df_colnames = list(df.columns)
+    df_colnames[idx_X] = 'X'
+    df.columns = df_colnames
+    return df
 
+def RunGenData(sample_N=12000, remember_seed = 1444260861):
+    # Data load
+    IST = pd.read_csv('IST.csv')
+    IST = ReduceIST(IST)
+
+    IST = IndexifyDisc(IST)
+    IST = ContToDisc(IST)
+
+    # remember_seed = SeedFinding(IST,sample_N=12000, alpha=0.01)
+
+    EXP = GenEXP(IST,sample_N,remember_seed)
+    OBS = GenOBS(EXP)
+
+    EXP, OBS = HideCovarOBS(EXP, OBS)
+    # EXP = ChangeRXASPtoX(EXP)
+    # OBS = ChangeRXASPtoX(OBS)
+    return [EXP,OBS]
+
+def QualityCheck(EXP,OBS,X,TF_emp = False,delta=0.01):
+    if TF_emp:
+        LB, HB = EmpiricalComputeBound(OBS, X,delta)
+    else:
+        LB, HB = ComputeBound(OBS, X)
+    U = GroundTruth(EXP, X)
+    TF_Case2 = CheckCase2(HB, U)
+
+    lx0, lx1 = LB
+    hx0, hx1 = HB
+    Ux0, Ux1 = U
+
+    print([lx0, Ux0, hx0])
+    print([lx1, Ux1, hx1])
+    return(TF_Case2)
+
+def drawPolicy(pl,Z):
+    prob0, prob1 = pl(Z[0],Z[1])
+    return np.random.binomial(n=1, p=prob1, size=1)[0]
+
+def SigTestPl(df,X, policy_list,alpha=0.01):
+    numPolicy = len(policy_list)
+    possible_pairs = list(itertools.combinations(range(numPolicy),2))
+
+    listOutcome = []
+    listStat = []
+
+    for pl in policy_list:
+        listOutcome.append(ExpectedOutcomePl(df, pl))
+        listStat.append(ttestStatGen(df,X,pl))
+
+    listPval = []
+    for pair_elem in possible_pairs:
+        nobs1,mean1,std1,seed1 = listStat[pair_elem[0]]
+        nobs2, mean2, std2,seed2 = listStat[pair_elem[1]]
+        result = ttest_ind_from_stats(mean1=mean1, std1=std1, nobs1=nobs1,
+                             mean2=mean2, std2=std2, nobs2=nobs2,
+                             equal_var=False)
+
+        listPval.append(result.pvalue)
+    arrayPval = np.array(listPval)
+    # print(possible_pairs)
+    print(arrayPval)
+    if sum((arrayPval < alpha) * 1) == len(listPval):
+        print(seed1,seed2)
+        return True
+    else:
+        return False
+
+def ExpectedOutcomePl(EXP,pl):
+    sum_prob = 0
+    for age in [0,1]:
+        for sex in [0,1]:
+            Pz = len(EXP[(EXP['AGE']==age)&(EXP['SEX']==sex)])/len(EXP)
+            probs = pl(age,sex)
+            Yz = EXP[(EXP['AGE'] == age) & (EXP['SEX'] == sex)]
+            for x in [0,1]:
+                Yx_z = Yz[Yz[X]==x]['Y']
+                EYx_z = np.mean(Yx_z)
+                sum_prob_val = EYx_z * probs[x] * Pz
+                # print(sum_prob_val, EYx_z, probs[x],Pz,age,sex)
+                sum_prob += sum_prob_val
+    return sum_prob
+
+def ttestStatGen(df,X,pl):
+    listSample = []
+    possible_case = 2 ** 32 - 1
+    seed_num = np.random.randint(possible_case)
+    # weight series construction
+    N = len(df)
+    ## pl success condition
+    # for age in [0,1]:
+    #     for sex in [0,1]:
+    #         prob0, prob1 = pl(age,sex)
+    #         if prob1 > prob0:
+    #             Z = [age,sex]
+    #             break
+    #
+    # n = len(df[(df['AGE']==Z[0]) & (df['SEX']==Z[1])])
+    # PB = 0.95
+    # M = int(np.round(N/5))
+    # listSampleProb = []
+    # for idx in range(len(df)):
+    #     elem_df = df.iloc[idx]
+
+    np.random.seed(seed_num)
+    for idx in range(len(df)):
+        elem_df = df.iloc[idx]
+        z = [elem_df['AGE'], elem_df['SEX']]
+        x = int(elem_df[X])
+        elem_probs = pl(z[0],z[1])
+        sample_prob = elem_probs[x]
+        if np.random.binomial(1, sample_prob, 1)[0] == 1:
+            listSample.append(elem_df)
+    dfSample = pd.DataFrame(listSample)
+    nobs = len(listSample)
+    mean_obs = np.mean(dfSample['Y'])
+    std_obs = np.std(dfSample['Y'])
+
+    return [nobs, mean_obs, std_obs, seed_num]
+
+def SeedFindingPl(IST, X, policy_list, sample_N=12000, alpha=0.01):
+    iter_idx = 0
+    possible_case = 2 ** 32 - 1
+
+    while 1:
+        iter_idx += 1
+        seed_num = np.random.randint(possible_case)
+        np.random.seed(seed_num)
+        Sample = IST.sample(n=sample_N)
+        TF_sig = SigTestPl(Sample, 'RXASP', policy_list, alpha)
+        print(iter_idx,TF_sig)
+        if TF_sig:
+            return seed_num
+
+
+
+# Load dataset
+IST = pd.read_csv('IST.csv')
 IST = ReduceIST(IST)
 
 IST = IndexifyDisc(IST)
 IST = ContToDisc(IST)
 
-EXP = GenEXP(IST)
-OBS = GenOBS(EXP)
+X = 'RXASP'
 
-print(ObsEffect(EXP,'Y'))
-print(ObsEffect(OBS,'Y'))
+# Define policies
+policy_list = []
 
-EXP,OBS = HideCovarOBS(EXP,OBS)
-LB,HB = ComputeBound(OBS,X)
-U = GroundTruth(EXP,X)
-TF_Case2 = CheckCase2(HB,U)
+low_prob = 0.1
+high_prob = 0.9
+pl1 = lambda age, sex: [low_prob, high_prob] if (age == 0) and (sex == 0) else [high_prob, low_prob]
+pl1_ = lambda age, sex: [high_prob, low_prob] if (age == 0) and (sex == 0) else [low_prob, high_prob]
 
-lx0, lx1 = LB
-hx0, hx1 = HB
-Ux0, Ux1 = U
+# pl2 = lambda age, sex: [low_prob, high_prob] if (age == 0) and (sex == 1) else [high_prob, low_prob]
+# pl3 = lambda age, sex: [low_prob, high_prob] if (age == 1) and (sex == 0) else [high_prob, low_prob]
+# pl4 = lambda age, sex: [low_prob, high_prob] if (age == 1) and (sex == 1) else [high_prob, low_prob]
 
-print([lx0,Ux0,hx0])
-print([lx1,Ux1,hx1])
+policy_list = [pl1,pl1_]
+# policy_list = [pl1,pl2,pl3,pl4]
+# policy_list = [pl1,pl1_]
 
+remember_seed = SeedFindingPl(IST,X,policy_list,sample_N=10000,alpha=0.01)
+# remember_seed = 3011694160
+EXP = GenEXP(IST,sample_N=10000,remember_seed=remember_seed)
